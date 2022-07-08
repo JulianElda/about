c4e678c0ef22f4ccc7d5461734e3414c19e9bea7
Julius Polar <julius.polar@protonmail.com>
2022-07-08 11:42:47 +0200

    updated styles

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.html", "./index.html"],
+  theme: {
+    fontFamily: {
+      serif: "Alegreya",
+    },
+  },
+};
